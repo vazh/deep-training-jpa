aa08178f526aabce85f6ba947e04459b1cd95687
Muhammad Hariz Faza <hariz.faza@asyst.co.id>
2024-02-23 14:55:20 +0700

    init(*)

---
+++ src/main/java/id/co/asyst/deep/training/springdatajpa/SpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package id.co.asyst.deep.training.springdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaApplication.class, args);
+    }
+
+}
